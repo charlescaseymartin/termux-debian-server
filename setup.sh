6ef291a0cdddf8df11b753576068b8f953e0a9e4
charlescaseymartin <charlescaseymartin@gmail.com>
2025-10-08 10:40:32 +0200

    (fix) removed extra quote in password command

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ proot-distro install --override-alias "$DISTRO_ALIAS" debian
 # login to debian
 INSTALL_CMD="apt-get update -y && apt-get upgrade -y; apt-get install -y openssh-server python vim"
 SSH_CONFIG_CMD="sed -i 's/#Port 22/Port 4723/' /etc/ssh/sshd_config; sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config"
-SERVER_PASSWORD_CMD="echo 'root:$SERVER_ROOT_PASSWORD' | chpasswd'"
+SERVER_PASSWORD_CMD="echo 'root:$SERVER_ROOT_PASSWORD' | chpasswd"
 SERVER_SETUP_CMD="$INSTALL_CMD; $SSH_CONFIG_CMD; $SERVER_PASSWORD_CMD; exit"
 proot-distro login --isolated "$DISTRO_ALIAS" -- /usr/bin/bash -c "$SERVER_SETUP_CMD"
 
